6bebf9fbc4bf48358fee05d664f60062a165db6d
yaNimotsi <yanimotsi@yanmdex.ru>
2021-02-01 22:35:14 +0300

    task 3

--- HomeTaskLesson5_3/Program.cs
+++ HomeTaskLesson5_3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HomeTaskLesson5_3
 {
@@ -6,7 +7,45 @@ namespace HomeTaskLesson5_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите набор числе в диапазоне от 0 до 255");
+            string userVal = Console.ReadLine();
+
+            byte[] byteArrNums;
+
+            if (!CheckUsrValForCorrect(userVal, out byteArrNums))
+            {
+                Console.WriteLine("Введена пустая строка или какое-то значение не удоволетворяет требованиям ввода");
+                return;
+            }
+
+            string filePath = @"E:\GeekBrains\Base C#\‪UserText.bin";
+
+            WriteNumsToBinaryFile(filePath, byteArrNums);
+        }
+
+        static bool CheckUsrValForCorrect(string userVal, out byte[] byteArrNums)
+        {
+            string[] arrNums = userVal?.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            byteArrNums = new Byte[arrNums.Length];
+            
+            if (arrNums.Length == 0) return false;
+
+            for (int i = 0; i < arrNums.Length; i++)
+            {
+                if (!Byte.TryParse(arrNums[i], out byte num))
+                {
+                    return false;
+                }
+                byteArrNums[i] = num;
+            }
+
+            return true;
+        }
+
+        static void WriteNumsToBinaryFile(string filePath, byte[] userVal)
+        {
+            File.WriteAllBytes(filePath, userVal);
         }
     }
 }

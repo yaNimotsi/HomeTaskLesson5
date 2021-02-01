b7c72c0ef017733d1cc20f327cc3f5fe62726e9b
yaNimotsi <yanimotsi@yanmdex.ru>
2021-02-01 21:51:56 +0300

    Add project files.

---
+++ HomeTaskLesson5_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace HomeTaskLesson5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите набор данных для последующего сохранения \n" +
+                "в текстовый файл.");
+            string userVal = Console.ReadLine();
+
+            WriteUserTextToFile(userVal);
+        }
+
+        static void WriteUserTextToFile(string userVal)
+        {
+            File.WriteAllText(@"E:\GeekBrains\UserText.txt", userVal, System.Text.Encoding.UTF8);
+        }
+    }
+}
---
+++ HomeTaskLesson5_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace HomeTaskLesson5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dNow = " " + DateTime.Now.ToString();
+            string filePath = @"E:\GeekBrains\UserText.txt";
+
+            WriteToFileCurrentDate(dNow);
+        }
+
+        static void WriteToFileCurrentDate(string date)
+        {
+            File.AppendAllText(@"E:\GeekBrains\UserText.txt", date, System.Text.Encoding.UTF8);
+        }
+    }
+}

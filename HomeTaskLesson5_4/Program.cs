a703352b95239e0a03e65f1b6ec321e3288bc2c3
yaNimotsi <yanimotsi@yanmdex.ru>
2021-02-02 22:30:01 +0300

    Start create task 5

---
+++ HomeTaskLesson5_4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace HomeTaskLesson5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFolder = @"E:\WorkFolder";
+            string pathFileToWrite = @"E:\GeekBrains\SubFoldersAndFiles.txt";
+
+            string[] AllSubFolders = GetAllFolder(pathFolder);
+
+            PrintFolderAndFiles(AllSubFolders, pathFileToWrite);
+            
+        }
+
+        static string[] GetAllFolder(string startFolderPath)
+        {
+            return Directory.GetDirectories(startFolderPath, "*", SearchOption.AllDirectories);
+        }
+
+        static string[] GetFilesInFolder(string folderPath)
+        {
+            string[] pathsToFiles = Directory.GetFiles(folderPath);
+
+            string[] nameFiles = new string[pathsToFiles.Length];
+            for (int i = 0; i < pathsToFiles.Length; i++)
+            {
+                nameFiles[i] = Path.GetFileName(pathsToFiles[i]);
+            }
+
+            return nameFiles;
+        }
+
+        static void PrintFolderAndFiles(string[] AllFolders, string pathFileToWrite)
+        {
+            File.WriteAllText(pathFileToWrite, "", Encoding.UTF8);
+            for (int i = 0; i < AllFolders.Length; i++)
+            {
+                if (AllFolders[i] == null) continue;
+                else
+                {
+                    string[] filesInFolder = GetFilesInFolder(AllFolders[i]);
+
+                    File.AppendAllText(pathFileToWrite, $"Текущая папка {AllFolders[i]}", Encoding.UTF8);
+
+                    if (filesInFolder.Length > 0)
+                    {
+                        File.AppendAllText(pathFileToWrite, "\nФайлы в указанной папке:\n", Encoding.UTF8);
+                        foreach (string str in filesInFolder)
+                        {
+                            File.AppendAllText(pathFileToWrite, $"{str}\n", Encoding.UTF8);
+                        }
+                        File.AppendAllText(pathFileToWrite, "\n", Encoding.UTF8);
+                    }
+                    else
+                    {
+                        File.AppendAllText(pathFileToWrite, "\nВ указанной папке нет файлов!\n", Encoding.UTF8);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HomeTaskLesson5_5/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.IO;
+
+
+namespace HomeTaskLesson5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (StartMenu())
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Стартовое меню приложения
+        /// </summary>
+        /// <returns></returns>
+        static bool StartMenu()
+        {
+            //1 добавить задачу(и)
+            //2 загрузить текущие задачи
+            //3 Отметить задачу, как выполненную
+            //4 записать массив задач в файл
+            //5 закрыть приложение
+
+            Console.WriteLine("Введите номер команды, который вы хотите выполнить:" +
+                "\n 1 Добавить задачу" +
+                "\n 2 Загрузить текущие задачи" +
+                "\n 3 Отметить задачу, как выполненную" +
+                "\n 4 Обновить массив задач" +
+                "\n 5 Закрыть приложение");
+            int userVal = UserVal();
+
+            switch (userVal)
+            {
+                case 1:
+                    AddTask();
+                    return true;
+                case 2:
+                    return true;
+                case 3:
+                    return true;
+                case 4:
+                    return true;
+                case 5:
+                    return false;
+                default:
+                    Console.WriteLine("\nВведено недопустимое значение. Следуйте инструкциям\n");
+                    return true;
+            }
+        }
+        /// <summary>
+        /// Возврат значения введенного пользователем для стартового меню
+        /// </summary>
+        /// <returns></returns>
+        static int UserVal()
+        {
+            int.TryParse(Console.ReadLine(), out int userVal);
+            return userVal;
+        }
+
+        /// <summary>
+        /// Возврат значения введенного пользователем для добавления ззадачи
+        /// </summary>
+        /// <returns></returns>
+        static string UserValToAddTask()
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// Добавление новых задач
+        /// </summary>
+        static void AddTask()
+        {
+            PrinTasksFormJson();
+
+            Console.WriteLine("Введите текст нового задания");
+        }
+
+        /// <summary>
+        /// Печать текущих задач в консоль
+        /// </summary>
+        static void PrinTasksFormJson()
+        {
+            if (!IsTasksClear()) return;
+            ToDo[] json = JsonSerializer.Deserialize<ToDo[]>("tasks.json");
+
+            for (int i = 0; i < json.Length; i++)
+            {
+                if (json[i].IsDone)
+                {
+                    Console.WriteLine($"{i + 1} \"+\" {json[i].Title}");
+                }
+                else
+                {
+                    Console.WriteLine($"{i + 1} {json[i].Title}");
+                }
+            }
+        }
+        /// <summary>
+        /// Проверка пуст ли файл задач
+        /// </summary>
+        /// <returns></returns>
+        static bool IsTasksClear()
+        {
+            long fileSize = new FileInfo("tasks.json").Length;
+            if (fileSize > 8) return true;
+            return false;
+        }
+    }
+}
